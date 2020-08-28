92f8573dc0241e443cdd18200f27cd0706d3c4a0
nelrsf <nel.rsf@gmail.com>
2020-08-27 20:08:11 -0500

    first commit

---
+++ conection/conect.php
@@ -0,0 +1,11 @@
+<?php
+   function connection(){
+       $user = "b6_26604057";
+       $pass = "123456";
+       $server = "sql104.byethost6.com";
+       $db = "b6_26604057_OTs";
+       $con = mysql_connect($server, $user, $pass) or die ("Error al conectar a la base de datos ".mysql_error());
+       mysql_select_db($db, $con) or die ("Error no se encuentra la base de datos ".mysql_error());
+       return $con;
+    }
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<h1>Conecting..</h1>
+<?php 
+   include("conection/main.php");
+?>
